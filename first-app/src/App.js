ba0c7f4c55976ba678cd28e6fdec6b3dffbc5f69
Андрей Клименченко <andreyklim@iMac-Andrej.local>
2021-05-04 19:24:53 +0300

    feat: add content and state

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,13 +1,59 @@
+import React from "react";
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpenFirst: false,
+      isOpenSecond: false,
+      isOpenThird: false,
+      isOpenFourth: false,
+      isOpenFifth: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClick(e, mode) {
+    e.stopPropagation();
+    this.setState((prevState) => ({
+      ...prevState,
+      [mode]: !prevState[mode]
+      }
+    ));
+  }
+  handleClose(e, mode) {
+    e.stopPropagation();
+    this.setState({
+      [mode]: false
+    });
+  }
+  render() {
+    return (
+      <div 
+        className="App" 
+        onClick={() => this.setState({      
+          isOpenFirst: false,
+          isOpenSecond: false,
+          isOpenThird: false,
+          isOpenFourth: false,
+          isOpenFifth: false,
+        })}
+      >
+        <Header 
+          isOpenFirst={this.state.isOpenFirst}
+          isOpenSecond={this.state.isOpenSecond}
+          isOpenThird={this.state.isOpenThird}
+          isOpenFourth={this.state.isOpenFourth}
+          isOpenFifth={this.state.isOpenFifth} 
+          handleClick={this.handleClick} handleClose={this.handleClose}/>
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
--- first-app/src/components/Header/Header.js
+++ first-app/src/components/Header/Header.js
@@ -2,80 +2,61 @@ import React from "react";
 import "./Header.css";
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpenFirst: false,
-      isOpenSecond: false,
-      isOpenThird: false,
-      isOpenFourth: false,
-      isOpenFifth: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(mode) {
-    this.setState((prevState) => ({
-      ...prevState,
-      [mode]: !prevState[mode]
-      }
-    ));
-  }
   render() {
     return (
       <header className="header">
         <div className="positioned">
-          <button className="header-btn" onClick={() => this.handleClick('isOpenFirst')}>
+          <button className="header-btn" onClick={(e) => this.props.handleClick(e, 'isOpenFirst')}>
             <span>Model S</span>
             </button>
-            {this.state.isOpenFirst ? (
+            {this.props.isOpenFirst ? (
               <div className="menu">
                 <p>Базовая модель S использует жидкостное охлаждение двигателя переменного тока, который имеет мощность 362 лошадиные силы...</p>
-                <button className="ok-btn" onClick={() => this.setState({isOpenFirst: false})}>ok</button>
+                <button className="ok-btn" onClick={(e) => this.props.handleClose(e, 'isOpenFirst')}>ok</button>
               </div>
             ) : null}
         </div>
         <div className="positioned">
-          <button className="header-btn positioned" onClick={() => this.handleClick('isOpenSecond')}>
+          <button className="header-btn positioned" onClick={(e) => this.props.handleClick(e, 'isOpenSecond')}>
             <span>Model X</span>
           </button>
-          {this.state.isOpenSecond ? (
+          {this.props.isOpenSecond ? (
             <div className="menu">
               <p>Первыми автомобиль получат покупатели, резервировавшие его более трёх лет назад и внёсшие депозит в 40 тысяч долларов. Предзаказ оформили около 25 тысяч человек, время ожидания поставок для новых покупателей — от 8 до 12 месяцев[4]. В Tesla ожидают, что половину их будущих продаж составят кроссоверы...</p>
-              <button className="ok-btn" onClick={() => this.setState({isOpenSecond: false})}>ok</button>
+              <button className="ok-btn" onClick={(e) => this.props.handleClose(e, 'isOpenSecond')}>ok</button>
             </div>
           ) : null}
         </div>
         <div className="positioned">
-          <button className="header-btn positioned" onClick={() => this.handleClick('isOpenThird')}>
+          <button className="header-btn positioned" onClick={(e) => this.props.handleClick(e, 'isOpenThird')}>
             <span>Model 3</span>
           </button>
-          {this.state.isOpenThird ? (
+          {this.props.isOpenThird ? (
             <div className="menu">
               <p>Базовая модель S использует жидкостное охлаждение двигателя переменного тока, который имеет мощность 362 лошадиные силы...</p>
-              <button className="ok-btn" onClick={() => this.setState({isOpenThird: false})}>ok</button>
+              <button className="ok-btn" onClick={(e) => this.props.handleClose(e, 'isOpenThird')}>ok</button>
             </div>
           ) : null}
         </div>
         <div className="positioned">
-          <button className="header-btn positioned" onClick={() => this.handleClick('isOpenFourth')}>
+          <button className="header-btn positioned" onClick={(e) => this.props.handleClick(e, 'isOpenFourth')}>
             <span>Model Y</span>
           </button>
-          {this.state.isOpenFourth ? (
+          {this.props.isOpenFourth ? (
             <div className="menu">
               <p>Базовая модель S использует жидкостное охлаждение двигателя переменного тока, который имеет мощность 362 лошадиные силы...</p>
-              <button className="ok-btn" onClick={() => this.setState({isOpenFourth: false})}>ok</button>
+              <button className="ok-btn" onClick={(e) => this.props.handleClose(e, 'isOpenFourth')}>ok</button>
             </div>
           ) : null}
         </div>
         <div className="positioned">
-          <button className="header-btn positioned" onClick={() => this.handleClick('isOpenFifth')}>
+          <button className="header-btn positioned" onClick={(e) => this.props.handleClick(e, 'isOpenFifth')}>
             <span>Roadster</span>
           </button>
-          {this.state.isOpenFifth ? (
+          {this.props.isOpenFifth ? (
             <div className="menu">
               <p>Базовая модель S использует жидкостное охлаждение двигателя переменного тока, который имеет мощность 362 лошадиные силы...</p>
-              <button className="ok-btn" onClick={() => this.setState({isOpenFifth: false})}>ok</button>
+              <button className="ok-btn" onClick={(e) => this.props.handleClose(e, 'isOpenFifth')}>ok</button>
             </div>
           ) : null}
         </div>
